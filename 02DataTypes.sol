9b9e799012d6d4cd0240ecd5c071e44afab33af7
lavisharma796 <lavisharma796@gmail.com>
2019-02-07 19:32:57 +0530

    Data types in Solidity

---
+++ 02DataTypes.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.1;
+
+contract MyContract {
+    string public stringValue= "myString";
+    bool public myBool= true;
+    int public myInt= -1;
+    uint public myUint= 1;
+    uint8 public myUint8= 8;
+}
